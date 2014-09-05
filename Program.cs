649a198916297144b740286f07b72ac34acb1163
jt222ii <jt222ii@student.lnu.se>
2014-09-05 14:46:55 +0200

    Added if conditionals for writing out amount of bills and coins as well as making sure that the amount of money entered is valid. Also made an option to restart or exit the program after a calculation has been made.

--- Program.cs
+++ Program.cs
@@ -10,12 +10,28 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-
+            start:
+            Console.Clear();
             //Totalsumman och belopp erhållet.
+        total:
+            
             Console.Write("Ange totalsumma        : ");
             double total = double.Parse(Console.ReadLine());
+
+            if (total < 1)
+            { Console.WriteLine("Felaktig totalsumma! {0:c} kan ej tolkas som en giltig summa pengar! vänligen försök igen.", total); //Konsolen skriver ut att det är fel och sedan återgår den till att man ska skriva in totalsumman
+            goto total;
+            }
+
+            recieved:
             Console.Write("Ange erhållet belopp   : ");
             double recieved = int.Parse(Console.ReadLine());
+            if (recieved < total)
+            {
+                Console.WriteLine("{0:c} täcker ej det belopp som förväntas betalas: {1:c} Vänligen försök  igen!", recieved, total); //Konsolen skriver ut att det är fel och sedan återgår den till att man ska skriva in det erhållna beloppet.
+                goto recieved;
+            }
+
 
             //Att betala efter öresavrundning och öresavrundningen.
             uint toPay = (uint)Math.Round(total, MidpointRounding.AwayFromZero); //MidpointRounding.AwayFromZero gör så att x.5 avrundar upp och inte ner.
@@ -32,26 +48,67 @@ namespace ConsoleApplication1
             uint five = (change % 10) / 5;
             uint one = (change % 5);
 
-
-            Console.WriteLine(fivehundred);
-            Console.WriteLine(hundred);
-            Console.WriteLine(fifty);
-            Console.WriteLine(twenty);
-            Console.WriteLine(ten);
-            Console.WriteLine(five);
-            Console.WriteLine(one);
+          
+           
 
             //kvitto
+            Console.WriteLine();
+            Console.WriteLine("KVITTO");
+            Console.WriteLine("----------------------------------------");
             Math.Round(roundOff, 0, MidpointRounding.AwayFromZero);
             Console.WriteLine("totalt                 : {0}", total);
             Console.WriteLine("öresavrundning         : {0:f2}", roundOff);
             Console.WriteLine("att betala             : {0}", toPay);
             Console.WriteLine("Kontant                : {0}", recieved);
             Console.WriteLine("Växel                  : {0}", change);
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine();
+
+            if (fivehundred > 0)
+            {
+                Console.WriteLine("500-lappar             : {0}",fivehundred);
+            }
+
+            if (hundred > 0)
+            {
+                Console.WriteLine("100-lappar             : {0}", hundred);
+            }
+
+            if (fifty > 0)
+            {
+                Console.WriteLine("50-lappar              : {0}", fifty);
+            }
+
+            if (twenty > 0)
+            {
+                Console.WriteLine("20-lappar              : {0}", twenty);
+            }
+
+            if (ten > 0)
+            {
+                Console.WriteLine("10-kronor              : {0}", ten);
+            }
+
+            if (five > 0)
+            {
+                Console.WriteLine("5-kronor               : {0}", five);
+            }
+
+            if (one > 0)
+            {
+                Console.WriteLine("1-kronor               : {0}", one);
+            }
 
 
+            Console.WriteLine("tryck ESC för att avsluta eller valfri tangent för att påbörja en ny uträkning!");
+            ConsoleKeyInfo buttonpress = Console.ReadKey();
+            if (buttonpress.Key == ConsoleKey.Escape)
+            { Environment.Exit(0); }
+            else
+            { goto start; }
 
         }
     }
 }
+
 
